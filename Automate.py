9d41e3fe2102be90af2a1eff7e3faaaa136778fe
Chad Lee <clee2@paylocity.com>
2023-03-24 11:26:31 -0500

    stashed, applied  updated

--- Automate.py
+++ Automate.py
@@ -1,4 +1,4 @@
 for i in range(3):
     print("hello Paylocity")
     print("added from develop branch")
-    print("another change frome remote branch , use stash")
+    print("another change frome remote branch , use stash","hello what's up",sep="-")
