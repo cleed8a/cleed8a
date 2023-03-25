f0d52549efc225515032358d9da6f71f34a5a0bf
CJ <changlee.d8a@gmail.com>
2023-03-25 10:28:25 -0500

    ytdownloader with progress bar

--- dlyoutube.py
+++ dlyoutube.py
@@ -1,11 +1,12 @@
 import pytube
+from pytube.cli import on_progress
 
 # Ask user for YouTube video URL and download location
 url = input("Enter YouTube video URL: ")
-download_path = input("Enter download location: ")
+download_path = r"C:\Users\chadl\Downloads"
 
 # Create YouTube object and download video
-yt = pytube.YouTube(url)
+yt = pytube.YouTube(url,on_progress_callback=on_progress)
 stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 stream.download(download_path)
 print("Video downloaded successfully to", download_path)
